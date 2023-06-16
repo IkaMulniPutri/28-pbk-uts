2c689c8f958e89504c9b41c6d0bd0ea095f09d72
IkaMulniPutri <ikamulniputri@student.uir.ac.id>
2023-06-17 00:20:27 +0700

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import About from '../views/AboutView.vue'
 import Data from '../views/DataView.vue'
+import Store from '../views/StoreView.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/data',
     name: 'data',
     component: Data
+  },
+  {
+    path: '/store',
+    name: 'store',
+    component: Store
   }
 ]
 
